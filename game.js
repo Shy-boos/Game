cdb6a34ba87c7bc03f3d7a78283eed93cbbb010d
Shy-boos <patriota1997@gmail.com>
2018-08-22 18:26:46 +0200

    H

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 function setup(){
-createCanvas(800,600)
-background(0)
+scale(0.1, 0.1);
+createCanvas(200,200);
+background(0);
 
+}
+function draw(){
+fill(255);
+rect(mouseX,mouseY,2,2,255);
 }
